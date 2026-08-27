3f957d08e4547f2f7c6589a4847f168b73e1cbbb
FlorianHESWALLIS <153815619+FlorianHESWALLIS@users.noreply.github.com>
2026-08-27 17:22:00 +0200

    fix: make applicants screen repositories injectable for tests

--- lib/views/jp_applicants_screen.dart
+++ lib/views/jp_applicants_screen.dart
@@ -10,16 +10,18 @@ import 'package:taf_match/utils/theme.dart';
 typedef _Applicant = ({String name, String photoUrl, double rating, int reviews});
 
 class ApplicantsScreen extends StatefulWidget {
-  const ApplicantsScreen({super.key, required this.job});
+  const ApplicantsScreen({super.key, required this.job, this.userRepository, this.reviewRepository,});
   final Job job;
+  final FirestoreUserRepository? userRepository;
+  final FirestoreReviewRepository? reviewRepository;
 
   @override
   State<ApplicantsScreen> createState() => _ApplicantsScreenState();
 }
 
 class _ApplicantsScreenState extends State<ApplicantsScreen> {
-  final _userRepository = FirestoreUserRepository();
-  final _reviewRepository = FirestoreReviewRepository();
+  late final FirestoreUserRepository _userRepository;
+  late final FirestoreReviewRepository _reviewRepository;
 
   @override
   void initState() {
@@ -27,6 +29,9 @@ class _ApplicantsScreenState extends State<ApplicantsScreen> {
     WidgetsBinding.instance.addPostFrameCallback((_) {
       context.read<ApplicationProvider>().listenToJobApplications(widget.job.id);
     });
+    _userRepository = widget.userRepository ?? FirestoreUserRepository();
+    _reviewRepository = widget.reviewRepository ?? FirestoreReviewRepository();
+  
   }
 
   Future<_Applicant> _loadApplicant(String studentId) async {
