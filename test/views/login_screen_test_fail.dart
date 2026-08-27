24aed1094b62cb77442fea2e755cd890ef7a134c
Discretos <152215212+Discretos2022@users.noreply.github.com>
2026-08-27 13:20:17 +0200

    Fix Test

--- test/views/login_screen_test_fail.dart
+++ test/views/login_screen_test_fail.dart
@@ -19,9 +19,11 @@ void main() {
     final authService = FakeAuthService();
     authService.signInError = 'Wrong password provided for that user.';
 
+    final userRepository = FakeUserRepository();
+
     await tester.pumpWidget(
       ChangeNotifierProvider(
-        create: (_) => AuthProvider(authService),
+        create: (_) => AuthProvider(authService, userRepository),
         child: const MaterialApp(home: LoginScreen()),
       ),
     );
@@ -29,7 +31,7 @@ void main() {
     await tester.enterText(find.byType(TextFormField).at(0), 'user@example.com');
     await tester.enterText(find.byType(TextFormField).at(1), 'secret123');
 
-    await tester.tap(find.widgetWithText(ElevatedButton, 'Login'));
+    await tester.tap(find.widgetWithText(FilledButton, 'Log in'));
     await tester.pumpAndSettle();
 
     expect(find.text('Wrong password provided for that user.'), findsOneWidget);
